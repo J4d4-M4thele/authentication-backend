53e8acd7157c8dac3786445cd3ce92e874d0e54d
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-25 16:57:37 +0200

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+//connect to database
 mongoose.connect(process.env.MONGO_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
@@ -16,8 +17,12 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log(`Couldn\'t connect to database: ${err}`)
 });
 
+//request results will be in json format
 app.use(express.json());
+//auth route
 app.use('/auth', require('./routes/user'));
+
+//port set
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> {
