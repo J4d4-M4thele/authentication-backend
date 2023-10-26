09b96dbea8a4cb202d2db762d51ac3e7ddb4dd18
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-26 09:26:09 +0200

    update

--- models/user.js
+++ models/user.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     email: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     password: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     }
-}, {timestamps: true});
+}, 
+{timestamps: true}
+);
 
 module.exports = mongoose.model('User', userSchema);
