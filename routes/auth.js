2ad5b4af40cc1c2a0e7d3da4803519b43adaa74b
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-26 09:38:20 +0200

    update

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,12 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 //registering user
-router.post('/auth/register', async (req,res)=> {
-    const {name, email, password} = req.body
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body
     try {
-        let user = await User.findOne({email});
-        if(user) {
-            return res.status(400).json({error: 'User already exists'})
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ error: 'User already exists' })
         }
         const hashed_password = await bcrypt.hash(password, 10);
         user = new User({
@@ -18,30 +18,37 @@ router.post('/auth/register', async (req,res)=> {
             password: hashed_password
         });
         await user.save();
-        return res.status(201).json({message: 'User created successfully!'})
+        return res.status(201).json({ message: 'User created successfully!' })
     } catch (err) {
         console.log(err)
     }
 });
 
 //logging user in
-router.post('/auth/login', async (req, res)=> {
-    const {email, password} = req.body;
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
     try {
         //test to see if user exists
-        let user= await User.findOne({email})
-        if(!user) {
-            return res.status(400).json({error: 'Invalid email address'});
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid email address' });
         }
-     //compare password for valid user
-     const isMatch = await bcrypt.compare(password, user.password);
-     if(!isMatch) {
-        return res.status(400).json({error: 'Invalid password'});
-    }
-    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
-        expiresIn: '1h'
-    });
-    return res.json(token);
+        //compare password for valid user
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ error: 'Invalid password' });
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        });
+        return res.status(200).json({
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            },
+        });
     } catch (err) {
         console.log(err.message)
     }
