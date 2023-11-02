9f4bd1ba809fd09efe768678f402bd1d060d593b
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-02 13:01:13 +0200

    update

--- index.js
+++ index.js
@@ -10,21 +10,30 @@ mongoose.connect(process.env.MONGO_URI, {
     useFindAndModify: false,
     useUnifiedTopology: true
 })
-.then(()=> {
-    console.log('Connected to database successfully!')
-})
-.catch((err)=> {
-    console.log(`Couldn\'t connect to database: ${err}`)
-});
+    .then(() => {
+        console.log('Connected to database successfully!')
+    })
+    .catch((err) => {
+        console.log(`Couldn\'t connect to database: ${err}`)
+    });
 
 //request results will be in json format
 app.use(express.json());
 //auth route
-app.use('/auth', require('./routes/auth'));
+app.use('api/auth', require('./routes/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    // get anything, load index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //port set
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
-   console.log(`Server is running on port ${PORT}.`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
 });
